0b1dd2718f19dd9c882f5ccada1b05b9182a7323
ogshas <shasog@gmail.com>
2023-12-04 10:38:21 +0200

    final

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+use Crwlr\Crawler\Steps\Dom;
+use Crwlr\Crawler\Steps\Html;
+use Crwlr\Crawler\Steps\Loading\Http;
+use Crwlr\Crawler\Stores\SimpleCsvFileStore;
+use MyCrawler\MyCrawler;
+
+require_once  __DIR__ . '/vendor/autoload.php';
+include 'src/MyCrawler.php';
+
+
+
+(new MyCrawler())
+    ->setStore(new SimpleCsvFileStore('./Store', 'barreau-angers'))
+    ->input('https://barreau-angers.org/annuaire-des-avocats/')
+    ->addStep(Http::get())
+    ->addStep(
+        Html::each('.container .liste_avocats [class^="avocat_vignette"]')
+        ->extract([
+            'Name' => Dom::cssSelector('.infos_avocat h3')->first()->innerText(),
+            'Link' => Dom::cssSelector('a')->first()->link(),
+        ])->addLaterToResult()
+    )
+    ->addStep(
+        Http::get()
+        ->keepInputData()
+        ->useInputKey('Link')
+        ->outputKey('Link-Response')
+    )
+    ->addStep(
+        Html::each('.avocat .container .row')
+        ->useInputKey('Link-Response')
+        ->keepInputData()
+    ->extract([
+            'Address' => Dom::cssSelector('.adresse')->text(),
+            'Restation de serment' => Dom::cssSelector('.w-50')->first()->innerText(),
+            'Email' => Dom::cssSelector('.email')->first()->innerText(),
+            'Phone' => Dom::cssSelector('.telephone')->first()->innerText(),
+    ])
+    ->addToResult([
+        'Address',
+        'Restation de serment',
+        'Email',
+        'Phone'
+    ])
+    )->runAndTraverse();
---
+++ src/MyCrawler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MyCrawler;
+
+use Crwlr\Crawler\Cache\FileCache;
+use Crwlr\Crawler\HttpCrawler;
+use Crwlr\Crawler\Loader\Http\HttpLoader;
+use Crwlr\Crawler\Loader\Http\Politeness\TimingUnits\MultipleOf;
+use Crwlr\Crawler\Loader\LoaderInterface;
+use Crwlr\Crawler\UserAgents\BotUserAgent;
+use Crwlr\Crawler\UserAgents\UserAgentInterface;
+use Psr\Log\LoggerInterface;
+
+class MyCrawler extends HttpCrawler
+{
+
+    protected function userAgent(): UserAgentInterface
+    {
+       return BotUserAgent::make('petalbot');
+    }
+
+
+    protected function loader(UserAgentInterface $userAgent, LoggerInterface $logger): LoaderInterface|array
+    {
+
+        $loader = new HttpLoader(
+            $userAgent ,
+            logger: $logger,
+            defaultGuzzleClientConfig: [
+                'verify' => false ,
+                'timeout' => 30,
+            ],
+        );
+
+        $loader
+            ->setCache(
+                ( new FileCache( __DIR__ . './../cache'))
+                ->ttl(31536000)
+                ->useCompression()
+            )
+            ->retryCachedErrorResponses()
+            ;
+
+        $loader
+            ->throttle()
+            ->waitBetween(new MultipleOf(10.0), new MultipleOf(15.0))
+            ;
+
+        return $loader;
+
+    }
+
+}
