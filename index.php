314b11130fb6de5c68aca0e26870cdfc75c08d35
ogshas <shasog@gmail.com>
2023-12-04 14:24:08 +0200

    refind Phone Number -- work only for one number

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ use Crwlr\Crawler\Steps\Dom;
 use Crwlr\Crawler\Steps\Html;
 use Crwlr\Crawler\Steps\Loading\Http;
 use Crwlr\Crawler\Stores\SimpleCsvFileStore;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberUtil;
 use MyCrawler\MyCrawler;
 
 require_once  __DIR__ . '/vendor/autoload.php';
@@ -39,6 +41,22 @@ include 'src/MyCrawler.php';
             'Phone' => Dom::cssSelector('.telephone')->first()->innerText(),
             'Site-Web' => Dom::cssSelector('.site_web')->first()->link(),
     ])
+            ->refineOutput('Phone', function (mixed $output) {
+                if (is_array($output)) {
+                    return $output;
+                }
+
+                $PhoneNumber = $output;
+                $phoneUtil = PhoneNumberUtil::getInstance();
+                try {
+                    $phoneNumberProto = $phoneUtil->parse($PhoneNumber, "FR");
+                    // Return the parsed phone number instead of dumping it
+                    return $phoneNumberProto;
+                } catch (NumberParseException $e) {
+                    // Handle parsing failure if needed
+                    return null; // or return an error message
+                }
+            })
     ->addToResult([
         'Address',
         'Restation de serment',
